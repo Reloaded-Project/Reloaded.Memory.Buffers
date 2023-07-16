c9ed491af8c46037b87ac94428de1817ab85312a
Sewer <seweratwork@gmail.com>
2023-07-16 20:57:58 +0100

    Fixed: Windows Build

--- src-rust/benches/my_benchmark.rs
+++ src-rust/benches/my_benchmark.rs
@@ -1,6 +1,8 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use pprof::criterion::{Output, PProfProfiler};
 use reloaded_memory_buffers::{buffers::Buffers, structs::params::BufferSearchSettings};
+
+#[cfg(not(target_os = "windows"))]
+use pprof::criterion::{Output, PProfProfiler};
 
 fn get_buffer() {
     let settings = BufferSearchSettings {
@@ -17,9 +19,18 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("get_buffer", |b| b.iter(get_buffer));
 }
 
+#[cfg(not(target_os = "windows"))]
 criterion_group! {
     name = benches;
     config = Criterion::default().with_profiler(PProfProfiler::new(1000, Output::Flamegraph(None)));
     targets = criterion_benchmark
 }
+
+#[cfg(target_os = "windows")]
+criterion_group! {
+    name = benches;
+    config = Criterion::default();
+    targets = criterion_benchmark
+}
+
 criterion_main!(benches);
