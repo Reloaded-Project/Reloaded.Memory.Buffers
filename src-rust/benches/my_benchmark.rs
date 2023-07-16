f661455ae238ecc3f3140e45f58265c8c756e5f1
sewer56lol <sewer56lol@googlemail.com>
2023-07-16 20:08:53 +0100

    Added: Linux Benchmarking Setup

---
+++ src-rust/benches/my_benchmark.rs
@@ -0,0 +1,25 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use pprof::criterion::{Output, PProfProfiler};
+use reloaded_memory_buffers::{buffers::Buffers, structs::params::BufferSearchSettings};
+
+fn get_buffer() {
+    let settings = BufferSearchSettings {
+        min_address: 0_usize,
+        max_address: i32::MAX as usize,
+        size: 4096,
+    };
+
+    // Automatically dropped.
+    let _item = Buffers::get_buffer(&settings).unwrap();
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    c.bench_function("get_buffer", |b| b.iter(get_buffer));
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().with_profiler(PProfProfiler::new(1000, Output::Flamegraph(None)));
+    targets = criterion_benchmark
+}
+criterion_main!(benches);
